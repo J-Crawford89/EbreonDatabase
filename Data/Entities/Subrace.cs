ea3fe3ac2ce656605573ab9ad08416addc2cf8ae
J-Crawford89 <J.Crawford0120@Gmail.com>
2020-04-30 19:22:32 -0400

    Add project files.

---
+++ Data/Entities/Subrace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Subrace
+    {
+        [Key]
+        public int SubraceId { get; set; }
+        public string SubraceName { get; set; }
+        public string SubraceDescription { get; set; }
+        public string Source { get; set; }
+        [ForeignKey("ParentRace")]
+        public int RaceId { get; set; }
+        public virtual Race ParentRace { get; set; }
+    }
+}
