356367427edb21da9d7a4f967fb1136f4c809094
Jon <j.crawford@gmail.com>
2020-05-01 13:35:17 -0400

    initialized services

--- API/Controllers/RaceController.cs
+++ API/Controllers/RaceController.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Models.RaceModels;
+using Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,23 +12,26 @@ namespace API.Controllers
 {
     public class RaceController : ApiController
     {
-        private readonly IRaceService _service;
+        private IRaceService _service;
         [HttpPost]
         public IHttpActionResult Create(RaceCreateModel raceToCreate)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
+            _service = new RaceService();
             _service.CreateRace(raceToCreate);
             return Ok();
         }
         [HttpGet]
         public IHttpActionResult GetDetail(int raceId)
         {
+            _service = new RaceService();
             return Ok(_service.GetRaceDetailById(raceId));
         }
         [HttpGet]
         public IHttpActionResult GetList()
         {
+            _service = new RaceService();
             return Ok(_service.GetRaces());
         }
         [HttpPut]
@@ -35,12 +39,14 @@ namespace API.Controllers
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
+            _service = new RaceService();
             _service.UpdateRace(raceToUpdate);
             return Ok();
         }
         [HttpDelete]
         public IHttpActionResult Delete(RaceDeleteModel raceToDelete)
         {
+            _service = new RaceService();
             _service.DeleteRace(raceToDelete);
             return Ok();
         }
